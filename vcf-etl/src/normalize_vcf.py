73168cdfb0db342eb7b04b39a32fba15c5d9eace
samuelmoijueh <samuel_moijueh@bio-rad.com>
2023-11-20 09:33:17 -0500

    clean up. PEP8 formatting.

--- vcf-etl/src/normalize_vcf.py
+++ vcf-etl/src/normalize_vcf.py
@@ -4,7 +4,7 @@ import tempfile
 import re
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('normalize_vcf')
+logger = logging.getLogger("normalize_vcf")
 
 
 def read_vcf(vcf_in: str) -> (pd.DataFrame, str):
@@ -16,19 +16,17 @@ def read_vcf(vcf_in: str) -> (pd.DataFrame, str):
 
     with open(vcf_in) as f:
         for line in f:
-            if line.startswith('##'):
+            if line.startswith("##"):
                 vcf_header += line
             else:
                 lines.append(line)
-
-    headers = lines[0].split('\t')
+    headers = lines[0].split("\t")
     headers[-1] = headers[-1].strip()
 
     df = pd.DataFrame(columns=headers)
 
     for i, line in enumerate(lines[1:]):
-        df.loc[i] = line.split('\t')[:10]
-
+        df.loc[i] = line.split("\t")[:10]
     return df, vcf_header
 
 
@@ -37,10 +35,9 @@ def write_vcf(vcf_file_meta_info: str, df: pd.DataFrame, vcf_out: str) -> None:
     Write VCF to file (vcf_out).
     """
     with tempfile.TemporaryDirectory() as temp:
-        df.to_csv(temp + 'data.vcf', sep='\t', index=False)
-
+        df.to_csv(temp + "data.vcf", sep="\t", index=False)
     # append the header from the original VCF file, to the transformed VCF file, and write to the specified vcf_out
-    with open(temp + 'data.vcf') as f, open(vcf_out, 'a+') as file_out:
+    with open(temp + "data.vcf") as f, open(vcf_out, "a+") as file_out:
         file_out.write(vcf_file_meta_info)
 
         for line in f:
@@ -48,23 +45,19 @@ def write_vcf(vcf_file_meta_info: str, df: pd.DataFrame, vcf_out: str) -> None:
 
 
 def filter_Q1(df: pd.DataFrame) -> pd.DataFrame:
-    return df[df['FILTER'].isin(['PASS', 'LowGQX'])]
+    return df[df["FILTER"].isin(["PASS", "LowGQX"])]
 
 
 def filter_Q2(df: pd.DataFrame) -> pd.DataFrame:
-    df.loc[:, 'INFO'] = df['INFO'].apply(lambda x: x.split(';'))
-    
+    df.loc[:, "INFO"] = df["INFO"].apply(lambda x: x.split(";"))
     filter_pattern = ["AF1000G"]
-
-    df.loc[:, 'INFO'] = filter_helper(df['INFO'], filter_pattern)
-
+    df.loc[:, "INFO"] = filter_helper(df["INFO"], filter_pattern)
     return df
 
 
 def filter_helper(values: list, filter_pattern: list) -> list:
     def filter_elements(element):
         return any(pattern in element for pattern in filter_pattern)
-
 
     # Assign INFO as '.' for records with missing allele frequency
     filtered_column = [
@@ -87,11 +80,12 @@ def filter_Q3(df: pd.DataFrame) -> pd.DataFrame:
         values_list = row["NA12878"].split(":")
 
         filtered_tags_row = [tag for tag in desired_tags if tag in tags_list]
-        filtered_values_row = [values_list[tags_list.index(tag)] for tag in filtered_tags_row]
+        filtered_values_row = [
+            values_list[tags_list.index(tag)] for tag in filtered_tags_row
+        ]
 
         filtered_tags.append(":".join(filtered_tags_row))
         filtered_values.append(":".join(filtered_values_row))
-
     # Update the original DataFrame with filtered tags and values
     df.loc[:, "FORMAT"] = filtered_tags
     df.loc[:, "NA12878"] = filtered_values
@@ -100,7 +94,7 @@ def filter_Q3(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def filter_Q4(df: pd.DataFrame) -> pd.DataFrame:
-    return df[df['ALT'] != '.']
+    return df[df["ALT"] != "."]
 
 
 def normalize_vcf(vcf_in: str, vcf_out: str) -> None:
@@ -113,47 +107,32 @@ def normalize_vcf(vcf_in: str, vcf_out: str) -> None:
     Q3. The FORMAT column should only include the following values "GT:AD:DP"
     Q4. The final file should only have variant rows with an alternate allele
     """
-
-    logger.info(f' Reading input VCF file: {vcf_in}')
+    logger.info(f" Reading input VCF file: {vcf_in}")
     _df, vcf_header = read_vcf(vcf_in)
-    assert _df.shape == (37,10)
+    assert _df.shape == (37, 10)
 
-    logger.info(' Retaining records with PASS or LowGQX.')
+    logger.info(" Retaining records with PASS or LowGQX.")
     _df_Q1 = filter_Q1(_df)
-    assert set(_df_Q1['FILTER']) == set(['PASS', 'LowGQX'])
+    assert set(_df_Q1["FILTER"]) == set(["PASS", "LowGQX"])
 
-    logger.info(' Filtering the INFO column for variant\'s allele frequency.')
+    logger.info(" Filtering the INFO column for variant's allele frequency.")
     _df_Q2 = filter_Q2(_df_Q1)
 
-    AF_match = r'AF1000G=0\.\d{6}'
-    assert _df_Q2['INFO'].str.contains(f'^.$|{AF_match}').all() == True
-
+    AF_match = r"AF1000G=0\.\d{6}"
+    assert _df_Q2["INFO"].str.contains(f"^.$|{AF_match}").all() == True
 
     logger.info(' Filtering the FORMAT column for the following values: "GT:AD:DP".')
     _df_Q3 = filter_Q3(_df_Q2)
-    
-    for sub_list in _df_Q3['FORMAT']:
-        assert all(x in ['GT','AD','DP'] for x in sub_list.split(':'))
 
+    for sub_list in _df_Q3["FORMAT"]:
+        assert all(x in ["GT", "AD", "DP"] for x in sub_list.split(":"))
     # check that FORMAT column and NA12878 column contain the same number of values
-    for i, j in zip(_df_Q3['FORMAT'], _df_Q3['NA12878']):
-        assert (len(list(i.split(':'))) == len(list(j.split(':'))))
-        
-
-    logger.info(' Retaining records that have an alternative allele.')
+    for i, j in zip(_df_Q3["FORMAT"], _df_Q3["NA12878"]):
+        assert len(list(i.split(":"))) == len(list(j.split(":")))
+    logger.info(" Retaining records that have an alternative allele.")
     _df_Q4 = filter_Q4(_df_Q3)
-    # print(_df_Q4['ALT'])
-    for i, j in zip(_df_Q4['REF'], _df_Q4['ALT']):
+    for i, j in zip(_df_Q4["REF"], _df_Q4["ALT"]):
         assert i != j
-        assert j != '.'
-
-    logger.info(f' Writing the normalized VCF to file: {vcf_out}.')
+        assert j != "."
+    logger.info(f" Writing the normalized VCF to file: {vcf_out}.")
     write_vcf(vcf_header, _df_Q4, vcf_out)
-
-
-# def main():
-#     normalize_vcf('../data/NA12878_cod_ex.genome.vcf', 'test.vcf')
-
-
-# if __name__ == "__main__":
-#     main()
--- vcf-etl/tests/test_normalize_vcf.py
+++ vcf-etl/tests/test_normalize_vcf.py
@@ -1,8 +1,16 @@
-from src.normalize_vcf import normalize_vcf, read_vcf, filter_Q1, filter_Q2, filter_Q3, filter_Q4
+from src.normalize_vcf import (
+    normalize_vcf,
+    read_vcf,
+    filter_Q1,
+    filter_Q2,
+    filter_Q3,
+    filter_Q4,
+)
 
 import tempfile
 import pytest
 import re
+
 
 @pytest.fixture
 def dataframe():
@@ -13,8 +21,7 @@ def test_vcf_reader(dataframe):
     """
     Check that .vcf was read in properly.
     """
-    assert dataframe.shape == (37,10)
-
+    assert dataframe.shape == (37, 10)
 
 
 def test_filter_Q1(dataframe):
@@ -22,7 +29,7 @@ def test_filter_Q1(dataframe):
     Check that FILTER column only contains PASS or LowGQX values.
     """
     df_Q1 = filter_Q1(dataframe)
-    assert set(filter_df['FILTER']) == set(['PASS', 'LowGQX'])
+    assert set(filter_df["FILTER"]) == set(["PASS", "LowGQX"])
 
 
 def test_filter_Q2(dataframe):
@@ -30,23 +37,21 @@ def test_filter_Q2(dataframe):
     Check that INFO column only contains AF values or a period value
     """
     df_Q2 = filter_Q2(dataframe)
-    assert df_Q2['INFO'].str.contains(f'^.$|{AF_match}').all() == True
+    assert df_Q2["INFO"].str.contains(f"^.$|{AF_match}").all() == True
 
 
 def test_filter_Q3(dataframe):
     """
-    test the 3rd filter function.
-    
+    Check that the the 3rd filter function works as expected.
     """
     df_Q3 = format_Q3(dataframe)
 
     # Check that FORMAT column only contains the desired values (GT, AD, DP)
-    for sub_list in _df_Q3['FORMAT']:
-        assert all(x in ['GT','AD','DP'] for x in sub_list.split(':'))
-
+    for sub_list in _df_Q3["FORMAT"]:
+        assert all(x in ["GT", "AD", "DP"] for x in sub_list.split(":"))
     # Check that FORMAT column and NA12878 column contain the same number of values
-    for i, j in zip(df_Q3['FORMAT'], df_Q3['NA12878']):
-        assert (len(list(i.split(':'))) == len(list(j.split(':'))))
+    for i, j in zip(df_Q3["FORMAT"], df_Q3["NA12878"]):
+        assert len(list(i.split(":"))) == len(list(j.split(":")))
 
 
 def test_filter_Q4(dataframe):
@@ -54,11 +59,14 @@ def test_filter_Q4(dataframe):
     Check that ALT column is not empty and not equal to the REF column
     """
     df_Q4 = filter_Q4(dataframe)
-    for i, j in zip(df_Q4['REF'], df_Q4['ALT']):
+    for i, j in zip(df_Q4["REF"], df_Q4["ALT"]):
         assert i != j
-        assert j != '.'
+        assert j != "."
 
 
 def test_normalize_vcf():
+    """
+    Check that hte normalize_vcf function works as expected.
+    """
     actual = normalize_vcf("file-in", "file-out")
-    assert actual != "Hello World"
+    assert actual == None
