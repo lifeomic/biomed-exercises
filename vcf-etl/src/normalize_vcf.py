acdd645dd0f2d277ceb9344ba55217cb42637427
Shawn <ssstrike@asu.edu>
2023-11-24 13:11:22 -0500

    add normalize code, add unit tests

--- vcf-etl/src/normalize_vcf.py
+++ vcf-etl/src/normalize_vcf.py
@@ -1,6 +1,85 @@
 import logging
+import pandas as pd
+import argparse
 
+# Specify input file when using argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument('-i',"--inputFile",help="File name of input VCF", type=str, default="../data/NA12878_cod_ex.genome.vcf")
+#parser.add_argument('-o',"--outputFile",help="File name of normalized VCF", type=str, default="../data/normalized.vcf")
+#args = parser.parse_args()
+#vcf_in = args.inputFile
+#vcf_out = args.outputFile
+
+def read_vcf(vcf_in: str):
+    with open(vcf_in,'rt') as inFile:
+        # Variable to hold metadata lines for vcf writing
+        metadataLines = []
+        for line in inFile:
+            # Pull metadata lines into list
+            if line.startswith("##"):
+                metadataLines.append(line)
+            # Pull header names for dataframe
+            elif line.startswith("#CHROM"):
+                headNames = line.strip("\n#").split("\t")
+                break
+    # Construct dataframe using header and ignore # lines
+    vcf = pd.read_csv(vcf_in, comment='#', delim_whitespace=True, header=None, names=headNames)
+    return(vcf, metadataLines)
+
+def write_vcf(vcf, metadata, vcf_out: str):
+    # Open write file and output metadata lines
+    with open(vcf_out,'wt') as outFile:
+        outFile.write("".join(metadata) + '#')
+    # Append write file with new vcf dataframe
+    vcf.to_csv(vcf_out, sep="\t", mode = 'a', index=False)
+    return()
+
+def filter_filter(vcf):
+    # Define dataframe to select PASS or LowGQX
+    vcf_filter = vcf[vcf["FILTER"].isin(["PASS","LowGQX"])]
+    return (vcf_filter)
+
+def filter_info(vcf):
+    # Split the elements of INFO into iterable elements
+    vcf["INFO"] = vcf["INFO"].transform(lambda x: x.split(';'))
+    # Apply list comprehension to filter for AF element
+    vcf["INFO"] = vcf["INFO"].transform(lambda x:[y for y in x if 'AF1000G' in y])
+    # Pull out of list
+    vcf["INFO"] = vcf["INFO"].transform(lambda x: ';'.join(x))
+    return (vcf)
+
+def filter_format(vcf):
+    # Split the elements of FORMAT into iterable elements
+    vcf["FORMAT"] = vcf["FORMAT"].transform(lambda x: x.split(':'))
+    # Apply intersection to filter for GT:AD:DP elements
+    vcf["FORMAT"] = vcf["FORMAT"].transform(lambda x: set(x).intersection({'GT','AD','DP'}))
+    # Pull out of set
+    vcf["FORMAT"] = vcf["FORMAT"].transform(lambda x: ':'.join(x))
+    return (vcf)
+
+def filter_allele(vcf):
+    # Define dataframe to select for ALT that is not a '.'
+    vcf_allele = vcf[vcf['ALT'] != '.']
+    return (vcf_allele)
 
 def normalize_vcf(vcf_in: str, vcf_out: str):
-    print("Hello World")
-    return "Hello World"
+    #Read VCF
+    vcf, metadata = read_vcf(vcf_in)
+    
+    #Remove any records that don't have a FILTER of "PASS" or "LowGQX"
+    vcf = filter_filter(vcf)
+    
+    #The INFO column should only contain this variant's specific allele frequency, i.e. "AF={actual value}"
+    vcf = filter_info(vcf)
+    
+    #The FORMAT column should only include the following values "GT:AD:DP"
+    vcf = filter_format(vcf)
+    
+    #The final file should only have variant rows with an alternate allele.
+    vcf = filter_allele(vcf)
+    
+    #Write out Normalized VCF
+    write_vcf(vcf, metadata, vcf_out)
+
+#testrun with argparse
+#normalize_vcf(vcf_in, vcf_out)
--- vcf-etl/tests/test_normalize_vcf.py
+++ vcf-etl/tests/test_normalize_vcf.py
@@ -1,6 +1,55 @@
-from src.normalize_vcf import normalize_vcf
+from src.normalize_vcf import normalize_vcf, write_vcf, read_vcf, filter_filter, filter_info, filter_format, filter_allele
 
+import pytest
+import os
 
-def test_normalize_vcf():
-    actual = normalize_vcf("file-in", "file-out")
-    assert actual == "Hello World"
+@pytest.fixture
+def testVCF():
+    return read_vcf("../data/NA12878_cod_ex.genome.vcf")[0]
+
+@pytest.fixture
+def testMetaData():
+    return read_vcf("../data/NA12878_cod_ex.genome.vcf")[1]
+
+def test_write_vcf(testVCF,testMetaData):
+    outputVCF = 'unitTest.vcf'
+    write_vcf(testVCF, testMetaData, outputVCF)
+    # Check if file was created
+    assert os.path.exists("unitTest.vcf")
+    
+def test_read_vcf(testVCF):
+    actual = testVCF
+    # Check shape to determine if read worked
+    assert actual.shape == (37,10)
+    
+def test_filter_filter(testVCF):
+    actual = filter_filter(testVCF)
+    # Check shape and FILTER contents
+    assert actual.shape == (30,10)
+    assert set(actual['FILTER']) == {'PASS','LowGQX'}
+    
+def test_filter_info(testVCF):
+    actual = filter_info(testVCF)
+    assert actual.shape == (37,10)
+    # Check for AF in INFO
+    af_Vals = [x for x in actual['INFO'] if "AF" in x]
+    assert len(af_Vals) > 0
+    # Check for multiple inputs in INFO
+    other_Vals = [x for x in actual['INFO'] if len(x.split(';')) > 1]
+    assert len(other_Vals) == 0
+
+def test_filter_format(testVCF):
+    actual = filter_format(testVCF)
+    assert actual.shape == (37,10)
+    # Check for inputs outside of the ref_Vals
+    ref_Vals = {'GT','AD','DP'}
+    format_Vals = [x for x in actual['FORMAT'] if len(set(x.split(':')).union(ref_Vals)) > 3]
+    assert len(format_Vals) == 0
+
+def test_filter_allele(testVCF):
+    actual = filter_allele(testVCF)
+    assert actual.shape == (18,10)
+    # Check for elements that contain a "." there should be none
+    alt_Vals = [x for x in actual['ALT'] if x == '.']
+    assert len(alt_Vals) == 0
+
