7a87af5a2d93eee7d998652b29b1d7f60badfbc3
Sviatoslav Kendall <slav@sviatoslavs-mbp.lan>
2023-12-02 03:18:14 -0500

    Adding my scripts, .toml file and a multi-sample VCF to highlight functionality for such files.

--- vcf-etl/src/normalize_vcf.py
+++ vcf-etl/src/normalize_vcf.py
@@ -1,6 +1,132 @@
 import logging
+import re
+import pandas as pd
 
+# Note:  https://samtools.github.io/hts-specs/VCFv4.1.pdf
+
+# READ VCF RETURN DATAFRAME AND METADATA
+def read_vcf(vcf_in: str):
+
+    # Extract metadata and colnames from vcf file
+    with open(vcf_in, 'rt') as file:
+        metadata = ''
+        colnames = []
+
+        for line in file:
+            if line.startswith('##'):
+                metadata = metadata + line
+
+            elif line.startswith('#CHROM'):
+                colnames = line.strip('#\n').split()
+                break
+        
+        # Extract data as dataframe
+        vcf = pd.read_csv(vcf_in, comment='#',header=None, names = colnames, delim_whitespace = True)
+    
+    return(vcf, metadata)
+
+# SCREEN BY FILTER VALUES
+def screen_filter(vcf: pd.DataFrame):
+    vcf_new = vcf[vcf["FILTER"].isin(["PASS","LowGQX"])]
+    return vcf_new 
+
+# SCREEN BY ALTERNATE ALLELE
+def screen_alt_allele(vcf: pd.DataFrame):
+    vcf_new = vcf.drop(vcf[vcf.ALT == '.'].index)
+    return vcf_new
+
+# TRIM INFO COLUMN
+def trim_info(vcf: pd.DataFrame):
+    vcf['INFO'] = vcf['INFO'].map(get_AF)
+    return vcf 
+
+# Helper function to extract allele frequency
+def get_AF(INFO: str):
+    AF = ''
+    af_pattern = r'AF1000G=([\d.]+)(;|$)'
+    af_match = re.search(af_pattern, INFO)
+    if af_match:
+        AF = 'AF1000G=' + af_match.group(1)
+    else:
+        AF = '.'
+
+    return AF
+
+
+# TRIM FORMAT COLUMN
+def trim_format(vcf: pd.DataFrame):
+    return vcf.apply(trim_format_rowfunc, axis=1)
+
+# Helper: apply per row
+def trim_format_rowfunc(row: pd.Series):
+    field_indexes = get_target_field_indexes(row['FORMAT'])
+    row[8:] = row[8:].map(lambda x: extract_by_index(x, field_indexes))
+    return row
+
+def get_target_field_indexes(FORMAT: str):
+    targets = ['GT','AD','DP']
+    format_fields = FORMAT.split(':')
+    indexes = list()
+    
+    # The target fields may not be all presents
+    for t in targets:
+        if t in format_fields:
+            indexes.append(format_fields.index(t))
+
+    return indexes
+
+# Helper function to extract specific field data from entries
+def extract_by_index(col: str, indexes: list):
+    fields = col.split(':')
+    return ":".join([fields[i] for i in indexes])
+
+
+def write_vcf(vcf: pd.DataFrame, metadata: str, vcf_out: str):
+    with open(vcf_out, 'wt') as file:
+        file.write(metadata + '#')
+    vcf.to_csv(vcf_out, sep = '\t', index = False, mode='a')
+
+    logging.debug(f"Wrote '{vcf_out}'")
+    return()
+
+def get_outfile(vcf_in: str, vcf_out: str):
+    if not vcf_out:
+        if vcf_in.endswith('.vcf'):
+            vcf_out = vcf_in[:-4] + '.normalized.vcf'
+        else:
+            vcf_out = vcf_in + '.normalized.vcf'
+
+    return vcf_out
 
 def normalize_vcf(vcf_in: str, vcf_out: str):
-    print("Hello World")
-    return "Hello World"
+
+    vcf_out = get_outfile(vcf_in, vcf_out)
+
+    vcf, metadata = read_vcf(vcf_in)
+    
+    vcf = screen_filter(vcf)
+    
+    vcf = screen_alt_allele(vcf)
+    
+    vcf = trim_info(vcf)
+    
+    vcf = trim_format(vcf)
+    
+    write_vcf(vcf, metadata, vcf_out)
+    return()
+
+# Run script solo with debug output
+if __name__ == "__main__":
+    import argparse
+    import pprint
+    pp = pprint.PrettyPrinter(indent=4)
+    FORMAT = "%(filename)s:%(funcName)s:%(lineno)s - %(message)s"
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser(description ='Normalize a VCF file.')
+
+    parser.add_argument('-i','--infile',  type = str, required=True, help='Path to VCF file to be normalized.')
+    parser.add_argument('-o', '--outfile', type = str, help = 'Path to where normalized VCF will be saved.')
+    args, positional = parser.parse_known_args()
+
+    normalize_vcf(args.infile, args.outfile)
--- vcf-etl/tests/test_normalize_vcf.py
+++ vcf-etl/tests/test_normalize_vcf.py
@@ -1,6 +1,101 @@
-from src.normalize_vcf import normalize_vcf
+import pandas as pd
+from src.normalize_vcf import (
+    normalize_vcf,
+    read_vcf,
+    screen_filter,
+    screen_alt_allele,
+    get_AF,
+    trim_info,
+    trim_format,
+    trim_format_rowfunc,
+    get_target_field_indexes,
+    extract_by_index,
+    write_vcf,
+    get_outfile,
+    normalize_vcf
+)
+import pytest
+
+filepath = "data/NA12878_cod_ex.genome.vcf"
+filepath_multicol = "data/Multi_sample_cod_ex.genome.vcf"
+
+def test_read_vcf():
+    df, metadata = read_vcf(filepath)
+    assert isinstance(df, pd.DataFrame)
+    assert isinstance(metadata, str)
 
 
-def test_normalize_vcf():
-    actual = normalize_vcf("file-in", "file-out")
-    assert actual == "Hello World"
+@pytest.fixture
+def vcf_dataframe():
+    df, _ = read_vcf(filepath)
+    return df
+
+
+# Fixture for testing functions that handle multi-sample VCF files
+#@pytest.fixture
+#def vcf_dataframe_multicol():
+#    df, _ = read_vcf(filepath_multicol)
+#    return df
+
+
+def test_screen_filter(vcf_dataframe):
+    filtered_df = screen_filter(vcf_dataframe)
+    n_post_filter = len(filtered_df.index)
+    assert (
+        n_post_filter == 30
+    ), "Incorrect number of variants removed from {} with 'FILTER == PASS or LowGQX'.".format(
+        filepath
+    )
+
+
+def test_screen_alt_allele(vcf_dataframe):
+    altered_df = screen_alt_allele(vcf_dataframe)
+    n_post_altered = len(altered_df.index)
+    assert (
+        n_post_altered > 0
+    ), "ALL variants removed from {} based on not having an alternate allele.".format(
+        filepath
+    )
+
+
+# Mock DataFrame for testing trim_info function
+@pytest.fixture
+def mock_dataframe():
+    data = {
+        "INFO": ["AF1000G=0.25;SomeOtherInfo", "AF1000G=0.75;SomeMoreInfo", "SomeInfo"]
+    }
+    return pd.DataFrame(data)
+
+
+def test_get_AF():
+    # Test the get_AF function
+    assert get_AF("AF1000G=0.25;SomeOtherInfo") == "AF1000G=0.25"
+    assert get_AF("AF1000G=0.75;SomeMoreInfo") == "AF1000G=0.75"
+    assert get_AF("SomeInfo") == "."
+
+def test_trim_info(vcf_dataframe):
+    trim_df = trim_info(vcf_dataframe)
+    test_value = ''.join(trim_df['INFO'][1:5].tolist())
+    expected_value  = '...AF1000G=0.494209'
+    assert test_value == expected_value
+
+# Mock DataFrame for testing trim_info function
+@pytest.fixture
+def mock_dataframe():
+    data = {
+        'INFO': ['SomeAnnotations;AF1000G=0.25', 'AF1000G=0.75;SomeMoreInfo', 'SomeInfo']
+    }
+    return pd.DataFrame(data)
+
+def test_get_AF():
+    # Test the get_AF function
+    assert get_AF('AF1000G=0.25;SomeOtherInfo') == 'AF1000G=0.25'
+    assert get_AF('AF1000G=0.75;SomeMoreInfo') == 'AF1000G=0.75'
+    assert get_AF('SomeInfo') == '.'
+
+def test_get_target_field_indexes():
+    assert get_target_field_indexes('FOO:GT:BAR:AD:BAZ:DP') == [1,3,5]
+
+def test_extract_by_index():
+    assert extract_by_index('FOO:GT:BAR:AD:BAZ:DP', [1,3,5]) == 'GT:AD:DP'
+
